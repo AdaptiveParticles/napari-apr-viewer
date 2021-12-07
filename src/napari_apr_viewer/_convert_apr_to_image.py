35ed2d17b181c1965ce9ae0f110cbcd34146be25
joeljonsson <jonsson@mpi-cbg.de>
2021-12-07 17:29:49 +0100

    add pixel reconstruction widget

---
+++ src/napari_apr_viewer/_convert_apr_to_image.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+import pyapr
+from magicgui import magic_factory
+
+if TYPE_CHECKING:
+    import napari
+
+
+@magic_factory(call_button="Run")
+def convert_apr_to_image(
+        layer: "napari.types.ImageData",
+        output_name: str = 'reconstruction_result'
+) -> "napari.types.LayerDataTuple":
+    """Reconstruct pixel image from an APR in a given layer"""
+    if not isinstance(layer, pyapr.APRSlicer):
+        print('Layer data is already a pixel image')
+        return None
+    par = layer.apr.get_parameters()
+    scale = [x * 2**(-layer.patch.level_delta) for x in (par.dz, par.dx, par.dy)]
+    return layer[:, :, :], {'name': output_name, 'scale': scale}, 'image'
--- src/napari_apr_viewer/_dock_widgets.py
+++ src/napari_apr_viewer/_dock_widgets.py
@@ -1,9 +1,10 @@
 from napari_plugin_engine import napari_hook_implementation
 from ._APRViewer import APRViewer
 from ._convert_image_to_apr import convert_image_to_apr
+from ._convert_apr_to_image import convert_apr_to_image
 from ._threshold import threshold
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return [APRViewer, convert_image_to_apr, threshold]
+    return [APRViewer, convert_image_to_apr, convert_apr_to_image, threshold]
