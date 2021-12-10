5732d7732899c9c560bff78f2af1423fe2b74397
joeljonsson <jonsson@mpi-cbg.de>
2021-12-10 11:25:17 +0100

    allow reading files with multiple sets of particles, returning one layer for each

--- src/napari_apr_viewer/_reader.py
+++ src/napari_apr_viewer/_reader.py
@@ -1,3 +1,4 @@
+import os.path
 import pyapr
 from typing import List, Any, Optional, Union, Tuple, Dict
 from napari.types import ReaderFunction
@@ -12,7 +13,7 @@ def napari_get_reader(path: Union[str, List[str]]) -> Optional[ReaderFunction]:
     Parameters
     ----------
     path : str or list of str
-        Path to a '.apr' file, or list of such paths.
+        Path to a '.apr' file, or a list of such paths.
 
     Returns
     -------
@@ -33,7 +34,10 @@ def napari_get_reader(path: Union[str, List[str]]) -> Optional[ReaderFunction]:
 
 def apr_reader(path: Union[str, List[str]]) -> List[Tuple[Any, Dict, str]]:
     """
-    Take a path or list of paths to '.apr' files and return a list of LayerData tuples.
+    Take a path or list of paths to '.apr' files and return a list of LayerData tuples, with one layer
+    for each particle field (channel) in each file. The returned layers are given names in the format
+    'file_name:particles_name'. Particle fields with 'segmentation' or 'label' in the name are returned
+    as 'labels' layers, unless the data is floating point. Remaining fields are returned as 'image' layers.
 
     Parameters
     ----------
@@ -50,18 +54,32 @@ def apr_reader(path: Union[str, List[str]]) -> List[Tuple[Any, Dict, str]]:
         (currently always 'image').
     """
 
-    def get_kwargs(apr, parts):
+    def get_kwargs(apr, parts, name, ltype='image'):
         par = apr.get_parameters()
-        return {'rgb': False,
-                'multiscale': False,
-                'contrast_limits': [parts.min(), parts.max()],
-                'scale': [par.dz, par.dx, par.dy]}
+        if ltype == 'image':
+            meta = {'name': name,
+                    'rgb': False,
+                    'multiscale': False,
+                    'contrast_limits': [parts.min(), parts.max()],
+                    'scale': [par.dz, par.dx, par.dy]}
+        else:
+            meta = {'name': name,
+                    'multiscale': False,
+                    'scale': [par.dz, par.dx, par.dy]}
+        return meta
 
     def read_apr(path: str):
-        apr, parts = pyapr.io.read(path)
-        return pyapr.data_containers.APRSlicer(apr, parts)
+        particle_fields = pyapr.io.get_particle_names(path)
+        apr = pyapr.io.read_apr(path)
+        base_name = path.split(os.path.sep)[-1].split('.')[0]
+        particle_data = [(pyapr.io.read_particles(path, parts_name=p), p) for p in particle_fields]
+        return [(pyapr.data_containers.APRSlicer(apr, parts),
+                 '{}:{}'.format(base_name, name),
+                 'labels' if (('label' in name or 'segmentation' in name)
+                              and not isinstance(parts, pyapr.FloatParticles)) else 'image')
+                for parts, name in particle_data]
 
     if isinstance(path, str):
         path = [path]
-    data = [read_apr(p) for p in path]
-    return [(dd, get_kwargs(dd.apr, dd.parts), 'image') for dd in data]
+    data = [x for p in path for x in read_apr(p)]
+    return [(dd, get_kwargs(dd.apr, dd.parts, name, ltype), ltype) for dd, name, ltype in data]
