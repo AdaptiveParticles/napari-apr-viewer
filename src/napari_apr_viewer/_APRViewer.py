490cf23e1577e85a6af71ca3716c940fd69c48b1
joeljonsson <jonsson@mpi-cbg.de>
2022-05-12 18:33:29 +0200

    update contrast limits on change to level mode

--- src/napari_apr_viewer/_APRViewer.py
+++ src/napari_apr_viewer/_APRViewer.py
@@ -73,6 +73,7 @@ class APRViewer(QWidget):
                 # set data type
                 if mode == 'level':
                     layer.data.dtype = np.uint8
+                    layer.contrast_limits = [0, layer.data.apr.level_max()]
                 else:
                     layer.data.dtype = pyapr.utils.particles_to_type(layer.data.parts)
 
